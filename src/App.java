9453cfd8bdb5de3f7a86e8429e8338e166d6fae7
Curro Portillo <211curroportillo@gmail.com>
2025-09-24 17:28:07 +0200

    cambio2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    String nombre = "Curro Portillo Guerrero";
+    String calle = "Calle Rodahuevos, 69"; 
+    String ciudad = "Guaro (Málaga)";
+    String pais = "España";
+    String telefono = "555 123456";
+    System.out.println (nombre);
+    System.out.printf ("%s - %s - %s%n", calle, ciudad, pais);
+    System.out.print (telefono); 
     }
 }
